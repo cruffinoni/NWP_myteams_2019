9181bded29ce88ad654983e66cf757949b2b715a
Clément RUFFINONI <clement.ruffinoni@epitech.eu>
2020-05-19 14:32:07 +0200

    Oh no

--- server/include/client/commands.h
+++ server/include/client/commands.h
@@ -41,6 +41,7 @@ static const struct commands_data_s CLIENT_COMMANDS[] = {
     {NULL, 0, NULL},
 };
 
+// Release event for create command
 uerror_t release_event_team(const client_t *client, const char **arg);
 uerror_t release_event_channel(const client_t *client, const char **arg);
 uerror_t release_event_thread(const client_t *client, const char **arg);
--- shared/communication/codes.h
+++ shared/communication/codes.h
@@ -47,28 +47,28 @@ typedef struct codes_data_s {
 } codes_data_t;
 
 static const codes_data_t CODES_DATA[] = {
-    {NOT_CONNECTED,       "The client is not connected"},
-    {ALREADY_LOGGED,      "The client is already logged"},
-    {INVALID_ARG_COUNT,   "Invalid argument count, got X expected Y"},
-    {LINE_TOO_LONG,       "Line too long"},
-    {COMMAND_NOT_FOUND,   "Command not found"},
-    {INTERNAL_ERROR,      "Server internal error"},
-    {INVALID_SYNTAX,      "One of the parameters provided is invalid"},
+    {NOT_CONNECTED, "The client is not connected"},
+    {ALREADY_LOGGED, "The client is already logged"},
+    {INVALID_ARG_COUNT, "Invalid argument count, got X expected Y"},
+    {LINE_TOO_LONG, "Line too long"},
+    {COMMAND_NOT_FOUND, "Command not found"},
+    {INTERNAL_ERROR, "Server internal error"},
+    {INVALID_SYNTAX, "One of the parameters provided is invalid"},
     {INVALID_ID_PROVIDED, "Invalid id provided"},
-    {ID_DOESNT_EXISTS,    "The provided unique ID is invalid"},
-    {ID_ALREADY_EXISTS,   "The provided unique ID already exists"},
-    {ARGUMENT_TOO_LONG,   "One of the provided arguments'"
-                          "length is too long"},
+    {ID_DOESNT_EXISTS, "The provided unique ID is invalid"},
+    {ID_ALREADY_EXISTS, "The provided unique ID already exists"},
+    {ARGUMENT_TOO_LONG, "One of the provided arguments'"
+                        "length is too long"},
     {TEAM_ALREADY_EXISTS, "The given team's name is already taken"},
     {CHANNEL_ALREADY_EXISTS, "The given channel's name is already taken"},
     {THREAD_ALREADY_EXISTS, "The given thread's name is already taken"},
 
-    {OK,                  "Command OK"},
+    {OK, "Command OK"},
     {CLIENT_CONTEXT_RESET, "Client contexts' has been reset"},
-    {SERVICE_READY,       "Connection established"},
-    {LOGIN_SUCCESSFUL,    "Logged successfully <uuid>"},
-    {DISCONNECTED,        "Client disconnected from server"},
-    {SERVICE_CLOSING,     "Remote service disconnected client"},
+    {SERVICE_READY, "Connection established"},
+    {LOGIN_SUCCESSFUL, "Logged successfully <uuid>"},
+    {DISCONNECTED, "Client disconnected from server"},
+    {SERVICE_CLOSING, "Remote service disconnected client"},
 
     {0, NULL}
 };
