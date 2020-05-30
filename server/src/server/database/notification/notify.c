46d4672b5325d3c7fc4dbd571d4b90a30e4baf00
Clément RUFFINONI <clement.ruffinoni@epitech.eu>
2020-05-30 18:07:47 +0200

    Herping the derp derp (silly scoping error)

---
+++ server/src/server/database/notification/notify.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** TODO: CHANGE DESCRIPTION.
+*/
+
+#include "server/database.h"
+#include "client/commands.h"
+#include "utils.h"
+
+//uerror_t db_notify_subscriber(const )
